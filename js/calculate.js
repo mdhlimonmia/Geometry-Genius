8c50ea9be654c045ce683432bf75f9b894990151
Mdhlimon <mdhlimonmia@gmail.com>
2023-02-18 19:16:23 +0600

    All Calculation Done

--- js/calculate.js
+++ js/calculate.js
@@ -5,10 +5,38 @@ const tngCalculate = document.getElementById('tng-btn').addEventListener("click"
     // console.log(area);
     addHistory('Triangle',area);
 })
-const doCalculate = document.getElementById('rng-btn').addEventListener("click",function(){
+const rngCalculate = document.getElementById('rng-btn').addEventListener("click",function(){
     const width = getValue('rng-width');
     const height = getValue('rng-height');
     const area = (width*height);  
     // console.log(area);
     addHistory('Rectangle',area);
+})
+const prgCalculate = document.getElementById('prg-btn').addEventListener("click",function(){
+    const width = getValue('prg-width');
+    const height = getValue('prg-height');
+    const area = (width*height);  
+    // console.log(area);
+    addHistory('Parallelogram',area);
+})
+const rbsCalculate = document.getElementById('rbs-btn').addEventListener("click",function(){
+    const width = getValue('rbs-width');
+    const height = getValue('rbs-height');
+    const area = (0.5*width*height);  
+    // console.log(area);
+    addHistory('Rhombus',area);
+})
+const penCalculate = document.getElementById('pen-btn').addEventListener("click",function(){
+    const width = getValue('pen-width');
+    const height = getValue('pen-height');
+    const area = (0.5*width*height);  
+    // console.log(area);
+    addHistory('Pentagon',area);
+})
+const elpCalculate = document.getElementById('elp-btn').addEventListener("click",function(){
+    const width = getValue('elp-width');
+    const height = getValue('elp-height');
+    const area = (3.1416*width*height);  
+    const finalArea = area.toFixed(2);
+    addHistory('Ellipse',finalArea);
 })
